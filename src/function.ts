8fd11bfc58ce6078a08790759f7e722de238d205
Sakif-Hridoy <sakifworkspace@gmail.com>
2025-12-26 23:43:41 +0600

    functions

---
+++ src/function.ts
@@ -0,0 +1,14 @@
+//Function
+// arrow function, normal function
+
+
+function addNormal(num1:number,num2:number): number{
+    return num1 + num2
+}
+
+
+console.log(addNormal(2,2))
+
+const addArrow = (num1:number,num2:number)=> num1 + num2;
+
+console.log(addArrow(5,6))
