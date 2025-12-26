a01aa4c1707a689a9b5d4f70c871968db0341a38
Sakif-Hridoy <sakifworkspace@gmail.com>
2025-12-26 23:52:01 +0600

    object types

--- src/function.ts
+++ src/function.ts
@@ -12,3 +12,18 @@ console.log(addNormal(2,2))
 const addArrow = (num1:number,num2:number)=> num1 + num2;
 
 console.log(addArrow(5,6))
+
+
+
+const poorUser = {
+    name:"Sakif",
+    balance:0,
+    addBalance(value:number){
+       const totalBalance =  this.balance + value;
+       return totalBalance;
+    }
+
+}
+
+
+console.log(poorUser.addBalance(150000))
